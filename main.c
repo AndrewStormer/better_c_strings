05f58819e5b0bde893a2dec6efe3bbbebb145681
Andrew Stormer <100243512+AndrewStormer@users.noreply.github.com>
2023-11-16 17:33:09 -0600

    Update main.c

--- main.c
+++ main.c
@@ -5,12 +5,12 @@
 
 
 int main(void) {
-    Bstr b = bstr_create("");
+    Bstr b = bstr_wrap("");
     bstr_print(b);
     bstr_append_str(&b, "my name is Andrew Stormer.");
     bstr_print(b);
     bstr_append_str(&b, " I   am going to split this paragraph by spaces. This code should work for any delim and any Bstr input into it.");
-    Bstr b2 = bstr_create(" This is going to be appended too.");
+    Bstr b2 = bstr_wrap(" This is going to be appended too.");
     bstr_append(&b, b2);
     printf("%d\n", bstr_strlen(b));
 
@@ -39,6 +39,6 @@ int main(void) {
     char * str = bstr_unwrap(b);
     puts(str);
 
-    return 0;    
+    return 0; 
 }
 
