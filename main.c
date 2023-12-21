be2b0502838bd690cd44b7ddbcec746e50849e2e
Andrew Stormer <100243512+AndrewStormer@users.noreply.github.com>
2023-12-21 17:43:58 -0600

    bstr -> bcs

--- main.c
+++ main.c
@@ -5,40 +5,49 @@
 
 
 int main(void) {
-    Bstr b = bstr_wrap("");
-    bstr_print(b);
-    bstr_append_str(&b, "my name is Andrew Stormer.");
-    bstr_print(b);
-    bstr_append_str(&b, " I   am going to split this paragraph by spaces. This code should work for any delim and any Bstr input into it.");
-    Bstr b2 = bstr_wrap(" This is going to be appended too.");
-    bstr_append(&b, b2);
-    printf("%d\n", bstr_strlen(b));
-
-    //Bstr b2 = bstr_substr(b.h, b.h + 5);
-    bstr_print(b);
+    Bcs b = bcs_wrap("Hello world ");
+    bcs_print(b);
+    bcs_append_str(&b, "my name is Andrew Stormer.");
     printf("%c\n", *b.nt);
-    // bstr_print(b2);
-    int count = bstr_count_char(b, 'l');
+    
+    bcs_print(b);
+    bcs_append_str(&b, " I   am going to split this paragraph by spaces. This code should work for any delim and any Bcs input into it.");
+    Bcs b2 = bcs_wrap(" This is going to be appended too.");
+    bcs_append(&b, b2);
+    printf("%d\n", bcs_strlen(b));
+
+    //Bcs b2 = bcs_subcs(b.h, b.h + 5);
+    bcs_print(b);
+    printf("%c\n", *b.nt);
+    // bcs_print(b2);
+    int count = bcs_count_char(b, 'l');
     printf("%d\n", count);
 
 
     int token_count;
-    Bstr * tokens = bstr_split(b,  ' ', &token_count, 1);
+    Bcs * tokens = bcs_split(b,  ' ', &token_count, 1);
 
-    // bstr_print_tokens(tokens, token_count);    
-    bstr_clean_tokens(&tokens, token_count);
-    //bstr_print_tokens(tokens, token_count);
-    bstr_reverse_tokens(&tokens, token_count);
-    //bstr_print_tokens(tokens, token_count);
-    bstr_reverse_tokens(&tokens, token_count);
+    //bcs_print_tokens(tokens, token_count);
+    printf("AAAA\n");
+    bcs_clean_tokens(&tokens, token_count)
+    //bcs_print_tokens(tokens, token_count);
+    bcs_reverse_tokens(&tokens, token_count);
+    //bcs_print_tokens(tokens, token_count);
+    bcs_reverse_tokens(&tokens, token_count);
     
-    Bstr joined_tokens = bstr_join(tokens, token_count);
-    bstr_print(joined_tokens);
-    //bstr_full_clean(&b);
-    bstr_print(b);
-    char * str = bstr_unwrap(b);
+    Bcs joined_tokens = bcs_join(tokens, token_count);
+    bcs_print(joined_tokens);
+    // bcs_full_clean(&b);
+    bcs_print(b);
+    char * str = bcs_unwrap(joined_tokens);
     puts(str);
+    Bcs name = bcs_wrap("name");
+    printf("%c\n", *b.nt);
+    
+    int pos = bcs_lmatch(b, name);
+    printf("%d\n", pos);
 
     return 0; 
 }
+
 
