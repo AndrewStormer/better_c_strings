981be0c9e5f808a1d0fe640613bd2c70c33ec6b6
Andrew Stormer <100243512+AndrewStormer@users.noreply.github.com>
2023-12-21 17:40:18 -0600

    bstr -> bcs

--- better_strings.h
+++ better_strings.h
@@ -2,42 +2,42 @@
 #define BETTER_STRINGS_H_
 
 
-#Include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 
 
-typedef struct bstr {
-  char * h, * nt; //head and null terminator (not actually saving space for '\0', we know we are at the end if a pointer points to Bstr.nt.
-} Bstr;
+typedef struct bcs {
+  char * h, * nt; //head and null terminator (not actually saving space for '\0', we know we are at the end if a pointer points to Bcs.nt.
+} Bcs;
 
-Bstr bstr_wrap(char * s);
-char * bstr_unwrap(Bstr b);
-void bstr_destroy(Bstr b);
-void bstr_destroy_tokens(Bstr * tokens, int token_count);
-Bstr bstr_substr(char * s, char * e);
-void bstr_print(Bstr b);
-void bstr_print_tokens(Bstr * tokens, int token_count);
-int bstr_strlen(Bstr b);
-//static Bstr bstr_resize(Bstr b, int new_len);
-void bstr_append_str(Bstr * b, char * s);
-void bstr_append(Bstr * b, Bstr s);
-void bstr_print(Bstr b);
-//static int bstr_count_tokens(Bstr b, char delim);
-//static char * bstr_next_token(Bstr b, char * h, char delim);
-Bstr * bstr_split(Bstr b, char delim, int * token_count, int keep_delim);
-Bstr bstr_join(Bstr * b, int token_count);
-void bstr_lclean(Bstr * b);
-void bstr_lclean_tokens(Bstr ** tokens, int token_count);
-void bstr_rclean(Bstr * b);
-void bstr_rclean_tokens(Bstr ** tokens, int token_count);
-void bstr_clean(Bstr * b);
-void bstr_clean_tokens(Bstr ** tokens, int token_count);
-void bstr_full_clean(Bstr * b);
-int bstr_count_char(Bstr b, char c);
-void bstr_reverse(Bstr * b);
-void bstr_reverse_tokens(Bstr ** tokens, int token_count);
-int bstr_lmatch(Bstr b, Bstr str);
+Bcs bcs_wrap(char * s);
+char * bcs_unwrap(Bcs b);
+void bcs_destroy(Bcs b);
+void bcs_destroy_tokens(Bcs * tokens, int token_count);
+Bcs bcs_subcs(char * s, char * e);
+void bcs_print(Bcs b);
+void bcs_print_tokens(Bcs * tokens, int token_count);
+int bcs_strlen(Bcs b);
+//static Bcs bcs_resize(Bcs b, int new_len);
+void bcs_append_str(Bcs * b, char * s);
+void bcs_append(Bcs * b, Bcs s);
+void bcs_print(Bcs b);
+//static int bcs_count_tokens(Bcs b, char delim);
+//static char * bcs_next_token(Bcs b, char * h, char delim
+Bcs * bcs_split(Bcs b, char delim, int * token_count, int keep_delim);
+Bcs bcs_join(Bcs * b, int token_count);
+void bcs_lclean(Bcs * b);
+void bcs_lclean_tokens(Bcs ** tokens, int token_count);
+void bcs_rclean(Bcs * b);
+void bcs_rclean_tokens(Bcs ** tokens, int token_count);
+void bcs_clean(Bcs * b);
+void bcs_clean_tokens(Bcs ** tokens, int token_count);
+void bcs_full_clean(Bcs * b);
+int bcs_count_char(Bcs b, char c);
+void bcs_reverse(Bcs * b);
+void bcs_reverse_tokens(Bcs ** tokens, int token_count);
+int bcs_lmatch(Bcs b, Bcs str);
 
 
 #endif //BETTER_STRINGS_H_
@@ -54,9 +54,8 @@ static int str_strlen(char * s) {
 }
 
 
-
-Bstr bstr_wrap(char * s) {
-  Bstr b;
+Bcs bcs_wrap(char * s) {
+  Bcs b;
   int len = str_strlen(s)-1;
   assert(len != 0 && "Cant wrap empty string!");
   b.h = malloc(sizeof(char)*len);
@@ -71,10 +70,10 @@ Bstr bstr_wrap(char * s) {
 
 
 //TODO: implement a way to declare in static space so we do not have to free
-//Problem: doing char str[bstr_strlen(b)+1] creates str in the stack, which does not persist after the function is returned from, and so we try to return a pointer to that stack space which changes when stack is used next.
+//Problem: doing char str[bcs_strlen(b)+1] creates str in the stack, which does not persist after the function is returned from, and so we try to return a pointer to that stack space which changes when stack is used next.
 //*** MUST FREE STRING PASSED BACK FROM FUNCTION
-char * bstr_unwrap(Bstr b) {
-  char * str = malloc(sizeof(char)*bstr_strlen(b) + 1);
+char * bcs_unwrap(Bcs b) {
+  char * str = malloc(sizeof(char)*bcs_strlen(b) + 1);
   char * c = b.h;
   char * s = str;
 
@@ -84,25 +83,25 @@ char * bstr_unwrap(Bstr b) {
     ++c;
   }
   *s = '\0';
-  bstr_destroy(b);
+  bcs_destroy(b);
   return &str[0];
 }
 
 
-inline void bstr_destroy(Bstr b) {
+inline void bcs_destroy(Bcs b) {
   free(b.h);
 }
 
 
-inline void bstr_destroy_tokens(Bstr * tokens, int token_count) {
+inline void bcs_destroy_tokens(Bcs * tokens, int token_count) {
   for (int i = 0; i < token_count; ++i)
-    bstr_destroy(tokens[i]);
+    bcs_destroy(tokens[i]);
 }
 
 
-Bstr bstr_substr(char * s, char * e) {
+Bcs bcs_subcs(char * s, char * e) {
   assert(e >= s);
-  Bstr b;
+  Bcs b;
   b.h = malloc(sizeof(char)*(e-s)+1);
   b.nt = b.h;
   char * c = s;
@@ -116,7 +115,7 @@ Bstr bstr_substr(char * s, char * e) {
 }
 
 
-void bstr_print(Bstr b) {
+void bcs_print(Bcs b) {
   char * c = b.h;
   while (c != b.nt + 1) {
     printf("%c", *c);
@@ -126,20 +125,20 @@ void bstr_print(Bstr b) {
 }
 
 
-void bstr_print_tokens(Bstr * tokens, int token_count) {
+void bcs_print_tokens(Bcs * tokens, int token_count) {
   for (int i = 0; i < token_count; i++) {
-    bstr_print(tokens[i]);
+    bcs_print(tokens[i]);
   }
 }
 	     
 
 //we do not keep an extra space for the null terminator
-inline int bstr_strlen(Bstr b) {
+inline int bcs_strlen(Bcs b) {
   return (b.h == NULL) ? 0 : b.nt - b.h + 1;
 }
 
 
-static Bstr bstr_resize(Bstr b, int new_len) {
+static Bcs bcs_resize(Bcs b, int new_len) {
   if (b.h != NULL) {
     char * s = malloc(sizeof(char)*new_len);
     assert(s != NULL);
@@ -150,7 +149,7 @@ static Bstr bstr_resize(Bstr b, int new_len) {
       ++i;
       ++c;
     }
-    bstr_destroy(b);
+    bcs_destroy(b);
     b.h = s;
     b.nt = b.h + new_len-1;
   } else {
@@ -161,9 +160,9 @@ static Bstr bstr_resize(Bstr b, int new_len) {
 }
 
 
-void bstr_append_str(Bstr * b, char * s) {
-  int len = bstr_strlen(*b), new_len = str_strlen(s)-1 + len;
-  *b = bstr_resize(*b, new_len);
+void bcs_append_str(Bcs * b, char * s) {
+  int len = bcs_strlen(*b), new_len = str_strlen(s)-1 + len;
+  *b = bcs_resize(*b, new_len);
   char * c1 = (b->h + (len)), * c2 = s;
   while (c1 <= b->nt) {
     *c1 = *c2;
@@ -174,9 +173,9 @@ void bstr_append_str(Bstr * b, char * s) {
 }
 
 
-void bstr_append(Bstr * b, Bstr s) {
-  int len = bstr_strlen(*b), new_len = bstr_strlen(s) + len;
-  *b = bstr_resize(*b, new_len);
+void bcs_append(Bcs * b, Bcs s) {
+  int len = bcs_strlen(*b), new_len = bcs_strlen(s) + len;
+  *b = bcs_resize(*b, new_len);
   char * c1 = (b->h + (len)), * c2 = s.h;
   while (c1 <= b->nt) {
     *c1 = *c2;
@@ -187,7 +186,7 @@ void bstr_append(Bstr * b, Bstr s) {
 }
 
 
-static int bstr_count_tokens(Bstr b, char delim) {
+static int bcs_count_tokens(Bcs b, char delim) {
   char * c = b.h;
   int count = 0;
   if (*c == delim)
@@ -202,7 +201,7 @@ static int bstr_count_tokens(Bstr b, char delim) {
 }
 
 
-static char * bstr_next_delim(Bstr b, char * h, char delim) {
+static char * bcs_next_delim(Bcs b, char * h, char delim) {
   while (*h != delim && h != b.nt) {
     ++h;
   }
@@ -210,7 +209,7 @@ static char * bstr_next_delim(Bstr b, char * h, char delim) {
 }
 
 
-static char * bstr_next_non_delim(Bstr b, char * h, char delim) {
+static char * bcs_next_non_delim(Bcs b, char * h, char delim) {
   while (*h == delim && h != b.nt) {
     ++h;
   }
@@ -219,31 +218,31 @@ static char * bstr_next_non_delim(Bstr b, char * h, char delim) {
 
 
 //token_count is passed by reference, when keep_delim is 1 we keep the last found delim (if multiple delims, it keeps all of them)
-Bstr * bstr_split(Bstr b, char delim, int * token_count, int keep_delim) {
-  *token_count = bstr_count_tokens(b, delim);
-  Bstr * tokens = malloc(sizeof(Bstr)**token_count);
+Bcs * bcs_split(Bcs b, char delim, int * token_count, int keep_delim) {
+  *token_count = bcs_count_tokens(b, delim);
+  Bcs * tokens = malloc(sizeof(Bcs)**token_count);
   assert(tokens != NULL);
   
   char * c = b.h, * d;
   for (int i = 0; i < *token_count; ++i) {
-    d = bstr_next_delim(b, c, delim);
+    d = bcs_next_delim(b, c, delim);
     if (keep_delim == 1 && d != b.nt) {
-      d = bstr_next_non_delim(b, d, delim);
+      d = bcs_next_non_delim(b, d, delim);
       --d;
     }
-    tokens[i] = bstr_substr(c, d - (1 - keep_delim));
-    c = bstr_next_non_delim(b, d, delim);
+    tokens[i] = bcs_subcs(c, d - (1 - keep_delim));
+    c = bcs_next_non_delim(b, d, delim);
   }
   return tokens;
 }
 
 
-Bstr bstr_join(Bstr * tokens, int token_count) {
+Bcs bcs_join(Bcs * tokens, int token_count) {
   int joined_len = 0;
   for (int i = 0; i < token_count; ++i)
-    joined_len += bstr_strlen(tokens[i]);
+    joined_len += bcs_strlen(tokens[i]);
   
-  Bstr b;
+  Bcs b;
   b.h = malloc(sizeof(char)*joined_len);
   assert(b.h != NULL);
   
@@ -262,12 +261,12 @@ Bstr bstr_join(Bstr * tokens, int token_count) {
 }
 
 
-void bstr_lclean(Bstr * b) {
+void bcs_lclean(Bcs * b) {
   char * c = b->h;
   while (*c == ' ') {
     ++c;
   }
-  Bstr nb = bstr_substr(c, b->nt);
+  Bcs nb = bcs_subcs(c, b->nt);
   char * temp = b->h;
   b->h = nb.h;
   b->nt = nb.nt;
@@ -276,20 +275,20 @@ void bstr_lclean(Bstr * b) {
 }
 
 
-void bstr_lclean_tokens(Bstr ** tokens, int token_count) {
+void bcs_lclean_tokens(Bcs ** tokens, int token_count) {
   for (int i = 0; i < token_count; ++i) {
-    bstr_lclean(&((*tokens)[i]));
+    bcs_lclean(&((*tokens)[i]));
   }
 }
 
 
-void bstr_rclean(Bstr * b) {
+void bcs_rclean(Bcs * b) {
   char * c = b->nt;
   while (*c == ' ') {
     --c;
   }
 
-  Bstr nb = bstr_substr(b->h, c);
+  Bcs nb = bcs_subcs(b->h, c);
   char * temp = b->h;
   b->h = nb.h;
   b->nt = nb.nt;
@@ -297,36 +296,36 @@ void bstr_rclean(Bstr * b) {
 }
 
 
-void bstr_rclean_tokens(Bstr ** tokens, int token_count) {
+void bcs_rclean_tokens(Bcs ** tokens, int token_count) {
   for (int i = 0; i < token_count; ++i) {
-    bstr_rclean(&((*tokens)[i]));
+    bcs_rclean(&((*tokens)[i]));
   }
 }
 
 
-void bstr_clean(Bstr * b) {
-  bstr_lclean(b);
-  bstr_rclean(b);
+void bcs_clean(Bcs * b) {
+  bcs_lclean(b);
+  bcs_rclean(b);
 }
 
 
-void bstr_clean_tokens(Bstr ** tokens, int token_count) {
+void bcs_clean_tokens(Bcs ** tokens, int token_count) {
   for (int i = 0; i < token_count; ++i) {
-    bstr_clean(&((*tokens)[i]));
+    bcs_clean(&((*tokens)[i]));
   }
 }
 
 
-void bstr_full_clean(Bstr * b) {
+void bcs_full_clean(Bcs * b) {
   int token_count = 0;
-  Bstr * tokens = bstr_split(*b,  ' ', &token_count, 0);
-  bstr_clean_tokens(&tokens, token_count);
-  *b = bstr_join(tokens, token_count);
-  bstr_destroy_tokens(tokens, token_count);
+  Bcs * tokens = bcs_split(*b,  ' ', &token_count, 0);
+  bcs_clean_tokens(&tokens, token_count);
+  *b = bcs_join(tokens, token_count);
+  bcs_destroy_tokens(tokens, token_count);
 }
 
 
-int bstr_count_char(Bstr b, char c) {
+int bcs_count_char(Bcs b, char c) {
   int count = 0;
   for (char * p = b.h; p <= b.nt; ++p) {
     if (*p == c)
@@ -336,7 +335,7 @@ int bstr_count_char(Bstr b, char c) {
 }
 
 
-void bstr_reverse(Bstr * b) {
+void bcs_reverse(Bcs * b) {
   char * s = b->h, * e = b->nt;
 
   while (e > s) {
@@ -349,21 +348,21 @@ void bstr_reverse(Bstr * b) {
 }
 
 
-void bstr_reverse_tokens(Bstr ** tokens, int token_count) {
+void bcs_reverse_tokens(Bcs ** tokens, int token_count) {
   for (int i = 0; i < token_count; ++i) {
-    bstr_reverse(&((*tokens)[i]));
+    bcs_reverse(&((*tokens)[i]));
   }
 }
 
 
 //TODO: rmatch_str, edit distance
-int bstr_lmatch(Bstr b, Bstr str) {
+int bcs_lmatch(Bcs b, Bcs str) {
   char * c = b.h, * s = str.h;
 
   while (c <= b.nt) {
     if (*s == *c) {
       if (s == str.nt)
-	return (c  - b.h) - bstr_strlen(str) + 1ss;
+	return (c  - b.h) - bcs_strlen(str) + 1ss;
       ++s;
     } else
       s = str.h;
